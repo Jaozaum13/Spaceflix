1761185831194dfe1a085e70c8d582a1a901a36c
Jaozaum <gacoutogomes@gmail.com>
2022-04-14 21:10:15 -0300

    Commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+$('.owl-one').owlCarousel({
+    loop:true,
+    margin:4,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+})
+
+$(document).ready(function(){
+    $('.owl-two').owlCarousel({
+        loop:true,
+        margin:4,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    })
+
+$(document).ready(function(){
+    $('.owl-three').owlCarousel({
+        loop:true,
+        margin:4,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    })
+
+$(document).ready(function(){
+    $('.owl-four').owlCarousel({
+        loop:true,
+        margin:4,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    })
+
+$(document).ready(function(){
+    $('.owl-five').owlCarousel({
+        loop:true,
+        margin:4,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    })
